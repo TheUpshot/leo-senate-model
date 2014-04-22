30e1af96c9e0a404a7fbebf3459e843b5dcebfb7
joshkatz <jakatz2@ncsu.edu>
2014-04-22 19:47:00 -0400

    changing default parameters

--- master-public.R
+++ master-public.R
@@ -1,4 +1,3 @@
-
 workingDir  <-  getwd()
 dataDir     <-  paste(workingDir, "data-publisher/", sep = "/")
 modelDir    <-  paste(workingDir, "model", sep = "/")
@@ -6,9 +5,9 @@ fundyDir    <-  paste(workingDir, "fundamentals", sep = "/")
 
 ### run the model
 setwd(modelDir)
-n.days <- 3      # number of days to sim. set to "all" to run all days. 
-just.today <- F  # if T, overrides n.days
-n.sims <- 250000
+n.days <- 30      # number of days to sim. set to "all" to run all days. 
+just.today <- T   # if T, overrides n.days
+n.sims <- 50000
 source("senate-model-2014.R")
 
 if (just.today) source("combine-data.R")
