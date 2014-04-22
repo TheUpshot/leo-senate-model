8741c58dbc656a524951a95e0b82d4d454bc8a8a
Amanda Cox <coxa@nytimes.com>
2014-04-22 17:17:56 -0400

    clearer defaults

--- master-public.R
+++ master-public.R
@@ -7,8 +7,8 @@ fundyDir    <-  paste(workingDir, "fundamentals", sep = "/")
 ### run the model
 setwd(modelDir)
 n.days <- 3      # number of days to sim. set to "all" to run all days. 
-just.today <- F  # if T, overrides n.days
-n.sims <- 20
+just.today <-   # if T, overrides n.days
+n.sims <- 250000
 source("senate-model-2014.R")
 
 if (just.today) source("combine-data.R")
