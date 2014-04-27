b31e6e5c15950d1e62dabdeb9857ac55bb4d377d
joshkatz <josh.katz@nytimes.com>
2014-04-27 10:21:25 -0400

    change combine-data to check for old files

--- model/combine-data.R
+++ model/combine-data.R
@@ -4,26 +4,30 @@ today <- format(today(), format = "%e-%b-%y")
 
 # update topline number
 filename <- paste0(wd, "senate-likelihood.tsv")
-df <- read.table(filename, header = T, as.is = T)
-df.new <- read.table(paste0(wd, "senate-likelihood-new.tsv"), header = T,
-                     as.is = T)
-if (tail(df$date, 1) == today) {
-  df <- df[-nrow(df), ]
+if (file.exists(filename)) {
+	df <- read.table(filename, header = T, as.is = T)
+	df <- df[df$date != today, ]
+	df.new <- read.table(paste0(wd, "senate-likelihood-new.tsv"), header = T,
+		as.is = T)
+	df.new$date[nrow(df.new)] <- today
+	df <- rbind(df, df.new)
+	write.table(df, file = filename, quote = F, sep='\t', row.names = F,
+		col.names = c("date", "dem"))
+} else {
+	file.copy(paste0(wd, "senate-likelihood-new.tsv"), to = filename)
 }
-df.new$date[nrow(df.new)] <- today
-df <- rbind(df, df.new)
-write.table(df, file = filename, quote = F, sep='\t', row.names = F,
-            col.names = c("date", "dem"))
 
 # update state numbers
 filename <- paste0(wd, "senate-likelihood-all.tsv")
-df <- read.table(filename, header = T, as.is = T)
-df.new <- read.table(paste0(wd, "senate-likelihood-all-new.tsv"), header = T,
-                     as.is = T)
-if (tail(df$date, 1) == today) {
-  df <- df[-nrow(df), ]
+if (file.exists(filename)) {
+	df <- read.table(filename, header = T, as.is = T)
+	df <- df[df$date != today, ]
+	df.new <- read.table(paste0(wd, "senate-likelihood-all-new.tsv"), header = T,
+		as.is = T)
+	df.new$date <- today
+	df <- rbind(df, df.new)
+	write.table(df, file = filename, quote = F, sep='\t', row.names = F,
+		col.names = c("office", "date", "dem"))
+} else {
+	file.copy(paste0(wd, "senate-likelihood-all-new.tsv"), to = filename)
 }
-df.new$date[nrow(df.new)] <- today
-df <- rbind(df, df.new)
-write.table(df, file = filename, quote = F, sep='\t', row.names = F,
-            col.names = c("office", "date", "dem"))
